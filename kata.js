ff11b754ef4bc3e997950d9c4f5f4f2a38799f7e
charlene bastians <charlenebastians@hotmail.com>
2018-08-28 10:54:57 +0100

    refactored to expose duplication

--- kata.js
+++ kata.js
@@ -5,19 +5,23 @@ const kata = {
         result += 'X'
         num -= 10
     }  
-    if (num >= 9) {
+    while (num >= 9) {
         result += 'IX'
         num -= 9
     }
-    if (num >=5) {
+    while (num >=5) {
         result += 'V'
         num -= 5
     }
-    if (num >= 4) {
+    while (num >= 4) {
         result += 'IV'
         num -= 4
     }
-    return result + 'I'.repeat(num)
+    while (num >= 1) {
+        result += 'I'
+        num -= 1
+    }
+    return result 
   }
 }
 
